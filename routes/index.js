da27d9fb8a3506c3330a59855cbcf36fa618e0eb
Kristof Goossens <kristof@gitlab.com>
2020-01-16 16:42:26 +0100

    [GHGL-1] No longer welcome

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GL Demo' });
+  res.render('index', { title: 'GL Demo with GH' });
 });
 
 module.exports = router;
